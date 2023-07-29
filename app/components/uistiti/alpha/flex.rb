b43ba46e7536d8783dff2b48477ea4871585ad0a
Tom Ecrepont <tomecrepont@gmail.com>
2023-07-29 17:12:24 +0200

    allow Flex to receive a html_options argument

--- app/components/uistiti/alpha/flex.rb
+++ app/components/uistiti/alpha/flex.rb
@@ -4,14 +4,15 @@ module Uistiti
   module Alpha
     class Flex < ViewComponent::Base
       renders_many :items
-      attr_reader :direction, :gap, :justify_content, :align_items
-      def initialize(direction: , gap: nil, justify_content: nil, align_items: nil)
+      attr_reader :direction, :gap, :justify_content, :align_items, :html_options
+      def initialize(direction: , gap: nil, justify_content: nil, align_items: nil, html_options: {})
         super()
         
         @direction = direction        
         @justify_content = justify_content        
         @align_items = align_items        
-        @gap = gap        
+        @gap = gap
+        @html_options = html_options        
       end
 
       private
@@ -22,7 +23,8 @@ module Uistiti
           "Flex--direction--#{direction}",
           ("Flex--gap--#{gap}" if gap),
           ("Flex--justify-content--#{justify_content}" if justify_content),
-          ("Flex--align-items--#{align_items}" if align_items)
+          ("Flex--align-items--#{align_items}" if align_items),
+          html_options[:class]
         ]
       end
     end
