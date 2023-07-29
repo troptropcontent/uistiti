bc11713d590158fc4fcbb7024468ed365d3caff8
Tom Ecrepont <tomecrepont@gmail.com>
2023-07-29 13:08:58 +0200

    setup lmain layout and controller

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  get '/home', to: 'pages#home', as: 'home'
+  get '/contact', to: 'pages#contact', as: 'contact'
+  get '/how_it_works', to: 'pages#how_it_works', as: 'how_it_works'
   mount Uistiti::Engine => "/uistiti"
 end
