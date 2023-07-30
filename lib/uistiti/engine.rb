f834e8307ce746069cf967769edc1d8ef8f53ef0
Tom Ecrepont <tomecrepont@gmail.com>
2023-07-30 17:41:58 +0200

    puts file

--- lib/uistiti/engine.rb
+++ lib/uistiti/engine.rb
@@ -6,7 +6,10 @@ module Uistiti
     isolate_namespace Uistiti
 
     #Show to lookbook where it can find the previews
-    Lookbook.config.preview_paths = [File.join(root, 'test/components/previews')]
+    puts '*'*80
+    puts File.join(Uistiti::Engine.root, 'test/components/previews')
+    puts '*'*80
+    Lookbook.config.preview_paths = [File.join(Uistiti::Engine.root, 'test/components/previews')]
     Lookbook.config.preview_layout = "uistiti/component_preview"
   end
 end
