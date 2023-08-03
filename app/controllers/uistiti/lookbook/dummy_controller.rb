72b56926b2b469be924c41e17fb05badc919ef02
Tom Ecrepont <tomecrepont@gmail.com>
2023-08-03 14:37:08 +0200

    create a #PUT /lookbook/dummy/:id endpoint that simulates a rest update

--- app/controllers/uistiti/lookbook/dummy_controller.rb
+++ app/controllers/uistiti/lookbook/dummy_controller.rb
@@ -11,8 +11,16 @@ module Uistiti
       #PUT /lookbook/dummy/error
       #GET /lookbook/dummy/error
       def error
-        puts params
         render status: 400, json: {:error => 'Bad request'}.to_json
+      end
+
+      #PUT /lookbook/dummy/:id
+      def update
+        if params[:status] == "error"
+          render status: 400, json: {:error => 'Bad request'}.to_json
+        else
+          render status: 200, json: {message: 'OK'}.to_json
+        end
       end
     end
   end
