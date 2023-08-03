a6f4e6d2ada8597bd0c08d08a59098725b534138
Tom Ecrepont <tomecrepont@gmail.com>
2023-08-03 14:38:54 +0200

    implement the new Togle component ⚡️

--- app/components/previews/uistiti/alpha/toggle_preview.rb
+++ app/components/previews/uistiti/alpha/toggle_preview.rb
@@ -4,17 +4,24 @@ module Uistiti
     class TogglePreview < ViewComponent::Preview
       # Toggle
       # ---------------
-      # On each changes, this component makes PUT request to @path with body sent as follow {@model: {@field: value}} where value is true/false for now.
-      # If the request retunr something else than a 2XX the toglle is reverted to its original state
+      # If a valid ActiveRecord model is provided with a valid field argument (the model must respond to a method of this field name)
+      # With a active record model the following assumtions are made : 
+      # - *path* : where the put request is sent is obtained with Rails.application.routes.url_helpers.send("#{model.model_name.singular_route_key}_path", model)
+      # - *checked* : the initial state of the toggle, is obtained with model.send(field)
+      # - *label_id* : the id of the label that wraps the toggle, is obtained with ActionView::RecordIdentifier.dom_id(model, 'toggle_label')
+      # - *input_id* : the id of the input that olds the toggle value is obtained with ActionView::RecordIdentifier.dom_id(model, 'toggle_input')
+      # - *param_key* : the param key sent in the body of the oput request is obntained with model.model_name.param_key
       #
-      # @param checked [Boolean]  
-      # @param path select [/uistiti/lookbook/dummy/success, /uistiti/lookbook/dummy/error]      
-      # @param model      
-      # @param field      
+      # All those assumptions can be overwritted if passed as options in the last arguments, ie Toggle.new(model, :active, label_id: 'foo') will generate a label with an id of foo
+      # 
+      # @param checked [Boolean]      
       # @param color_on  select [disabled, success, danger]    
       # @param color_off select [disabled, success, danger]
-      def playground(checked: false, path: '/uistiti/lookbook/dummy/success', model: 'automated_ticket', field: 'active', color_on: :success, color_off: :disabled)
-        render(Uistiti::Alpha::Toggle.new(checked:, path:, model:, field:, color_on:, color_off:))
+      # @param request_status select [success, error]     
+      def playground(checked: false, color_on: :success, color_off: :disabled, request_status: :success)
+        dummy_model = Uistiti::Lookbook::Dummy.new(active: checked, id: 25)
+        dummy_path = "/uistiti/lookbook/dummy/#{dummy_model.id}?status=#{request_status}"
+        render(Uistiti::Alpha::Toggle.new(dummy_model, :active, path: dummy_path,  color_on: , color_off:))
       end
     end
   end
--- app/components/uistiti/alpha/toggle.rb
+++ app/components/uistiti/alpha/toggle.rb
@@ -3,37 +3,60 @@
 module Uistiti
   module Alpha
     class Toggle < ViewComponent::Base
-      def initialize(checked: , path:, model:, field:, color_on: :success, color_off: :disabled)
+      attr_reader :field, :model, :options
+      def initialize(model, field, options = {})
         super()
         
-        @checked = checked
-        @path = path        
-        @model = model        
-        @field = field        
-        @color_off = color_off        
-        @color_on = color_on        
+        @model = model
+        @field = field   
+        @options = options    
       end
 
       private
 
+      def checked
+        options[:checked] || model.send(field)
+      end
+
+      def path
+        options[:path] || Rails.application.routes.url_helpers.send("#{model.model_name.singular_route_key}_path", model)
+      end
+      
+      def label_id
+        options[:label_id] || ActionView::RecordIdentifier.dom_id(model, 'toggle_label')
+      end
+
+      def input_id
+        options[:input_id] || ActionView::RecordIdentifier.dom_id(model, 'toggle_input')
+      end
+      
+      def param_key
+        options[:param_key] || model.model_name.param_key
+      end
+
+      def color_off
+        options[:color_off] || :disabled
+      end
+
+      def color_on
+        options[:color_on] || :success
+      end
+      
       def button_classes
         [
           "Toggle__button",
-          "Toggle__button--off--#{@color_off}",
-          "Toggle__button--on--#{@color_on}",
+          "Toggle__button--off--#{color_off}",
+          "Toggle__button--on--#{color_on}",
         ]
       end
 
-      def input_name
-        "toggle-#{@model}-#{@field}"
-      end
 
       def wrapper_data_set
         {
           controller: 'uistiti--alpha--toggle',
-          'uistiti--alpha--toggle-url-value':  @path,
-          'uistiti--alpha--toggle-model-value': @model,
-          'uistiti--alpha--toggle-field-value': @field
+          'uistiti--alpha--toggle-url-value':  path,
+          'uistiti--alpha--toggle-param-key-value': param_key,
+          'uistiti--alpha--toggle-field-value': field
         }
       end
     end
